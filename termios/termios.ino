9474c2e368393e19ca778660d19570202701a559
gani0325 <lge0325@bme.gachon.ac.kr>
2023-11-08 16:51:53 +0900

    ➕[Feat] termios 의 테스트 ino 파일

--- termios/termios.ino
+++ termios/termios.ino
@@ -1,26 +1,29 @@
-#include <string.h>
-
-void setup()
-{
-    Serial.begin(9600);
+int incomingByte = 0;    // for incoming serial data
+int16_t timed_delay=0;
+int16_t timed_count=0;
+void setup() {
+    Serial.begin(9600);    // opens serial port, sets data rate to 9600 bps
 }
 
-void loop()
-{
-
-    //Serial.print("hi");  
-    if(Serial.available()) {
-        char data[8];
-        char buf;
-        
-        for (int i = 0; i < 8; i++) {
-            buf = Serial.read();
-            //Serial.print("hi");
-            data[i] = buf;
-            
-        }
-        Serial.print(data);
+void loop() {
+  
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+  
+    // read the incoming byte:
+    incomingByte = Serial.read();
+  
+    // say what you got:
+    Serial.print((char)incomingByte);
+  }
+  timed_delay++;
+  if(timed_delay==INT16_MAX){
+    if(timed_count==10){
+      Serial.print("time\r\n");
+      timed_count=0;
     }
-
-    delay(100);
+    timed_count++;
+    timed_delay=0;
+  }
+  
 }
